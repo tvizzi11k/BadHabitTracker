63169749b06eff6896988b0b9fb552282eb613d5
tvizzi11k <tvizzigangbeathoe@mail.ru>
2024-09-19 17:51:23 +0300

    add db config + create db

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.22.4
 require (
 	github.com/jinzhu/gorm v1.9.16 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 )
--- utils/config.go
+++ utils/config.go
@@ -1,1 +1,29 @@
 package utils
+
+import (
+	"fmt"
+	"github.com/jinzhu/gorm"
+	"github.com/joho/godotenv"
+	models_ "habit-tracker/models "
+	"os"
+)
+
+var DB *gorm.DB
+
+func ConnectDB() {
+	err := godotenv.Load()
+	if err != nil {
+		panic("Error loading .env file")
+	}
+
+	dbURL := fmt.Sprintf("host=%s user=%s password=%s dbname=%s",
+		os.Getenv("DB_HOST"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"))
+
+	db, err := gorm.Open("postgres", dbURL)
+	if err != nil {
+		panic("failed to connect db")
+	}
+
+	db.AutoMigrate(&models_.User{}, &models_.Habit{}, &models_.Tracking{})
+	DB = db
+}
